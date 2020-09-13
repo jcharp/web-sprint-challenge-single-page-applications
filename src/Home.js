c68f733a85a1ce22b8a38704c8cb9b4b66345f43
unknown <jackcharp69@gmail.com>
2020-09-13 19:45:39 -0400

    added pizza and home component while starting to add route

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+
+
+const Home =() =>{
+    return(
+        <h1>PIZZA WHILE YOU STUDY</h1>
+    )
+    
+};
+
+export default Home;
---
+++ src/Pizza.js
@@ -0,0 +1,10 @@
+
+const Pizza = () => {
+    return(
+        <h1>place holder</h1>
+
+    )
+
+}
+
+export default Pizza;
