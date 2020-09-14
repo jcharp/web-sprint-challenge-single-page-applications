674ac006654af0ecc46cb1c7b90820311a460502
unknown <jackcharp69@gmail.com>
2020-09-13 21:31:58 -0400

    trying to get it render

--- src/Pizza.js
+++ src/Pizza.js
@@ -5,9 +5,9 @@ import ReactDom from 'react-dom';
 
 
 const Pizza = () => {
-    const [name, setName]= useState("");
-    const [size, setSize]= useState("");
-    const [toppings, setToppings]= useState("");
+    const [name, setName]= useState('');
+    const [size, setSize]= useState('');
+    const [toppings, setToppings]= useState('');
 
     const formSchema = yup.object().shape({
         name: yup.string().required('Name must be at least two characters')
@@ -16,44 +16,40 @@ const Pizza = () => {
     return(
         <>
             <h2>Create Your Personalized Pizza</h2>
-
+        <form onSubmit={event =>{
+            event.preventDefault();
+        }}>
+               
             <label htmlFor="name">Name </label>
             <input onChange={event => {
                 setName(event.target.value)
-                console.log.apply('this is the name', name);
+                console.log('this is the name', name);
             }}
                 type="text"
                 minLength="2"
                 placeholder='Name'
                 id='name'
+                value={name}
                 />
             <br/>
 
             <label htmlFor='size'>Coose your Pizza size </label>
             <select onChange= {event => {
                 setSize(event.target.value);
-                console.log('this is the pizza size',size)
+                console.log('this is the pizza size', size)
             }}>
 
                 <option>Large Pie</option>
                 <option>Medium Pie</option>
                 <option>Small Pie</option>
                 <option>Personal Pizza</option>
-
+                
 
             </select>
 
             <br/>
 
-            <label htmlFor='toppings'>Choose your Toppings </label><br/>
-
-            <label htmlFor='pepperoni'>Pepperoni</label>
-            <input onChange={event => {
-                if(event.target.checked) {setToppings(toppings + "pepperoni")}
-            }}
-                type='checkbox'
-                id='pepperoni'/>
-             <br/>
+           
 
             <label htmlFor='toppings'>Choose your Toppings </label><br/>
 
@@ -101,7 +97,9 @@ const Pizza = () => {
             <button type='submit'>Add to Order</button>
             
 
-        
+         
+
+        </form>
 
         </>
 
